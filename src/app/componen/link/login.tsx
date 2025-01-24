9406a8d543ae77663261c9d65ae308a857f695cc
badryansah <badryansah99@gmail.com>
2025-01-24 23:08:10 +0800

    login

---
+++ src/app/componen/link/login.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Image from 'next/image';
+import RandomGoofyAhhShit from '@/app/aset/shopping_cart_and_phone.jpg';
+
+export function Login() {
+  // ... kode login yang sudah ada ...
+}
